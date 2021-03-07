37becbe7a3cd5d91c5de1e7ac839ef896d54fff8
Sephinor <w229084881@vip.qq.com>
2021-03-07 16:06:47 +0800

    排除数据库依赖

--- kanglong-web/src/main/java/com/sephinor/kanglong/kanglongweb/KanglongWebApplication.java
+++ kanglong-web/src/main/java/com/sephinor/kanglong/kanglongweb/KanglongWebApplication.java
@@ -2,8 +2,9 @@ package com.sephinor.kanglong.kanglongweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class KanglongWebApplication {
 
 	public static void main(String[] args) {
