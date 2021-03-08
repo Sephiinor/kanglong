fb868c1516f7de64ebf3d2b1aa434fe2afcc1196
Sephinor <w229084881@vip.qq.com>
2021-03-08 19:44:53 +0800

    web模块集成service-api

--- kanglong-web/src/main/java/com/sephinor/kanglong/KanglongWebApplication.java
+++ kanglong-web/src/main/java/com/sephinor/kanglong/KanglongWebApplication.java
@@ -1,10 +1,13 @@
-package com.sephinor.kanglong.kanglongweb;
+package com.sephinor.kanglong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableFeignClients(basePackages = "com.sephinor.kangkong.service.api")
 public class KanglongWebApplication {
 
 	public static void main(String[] args) {
--- kanglong-web/src/test/java/com/sephinor/kanglong/KanglongWebApplicationTests.java
+++ kanglong-web/src/test/java/com/sephinor/kanglong/KanglongWebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sephinor.kanglong.kanglongweb;
+package com.sephinor.kanglong;
 
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
