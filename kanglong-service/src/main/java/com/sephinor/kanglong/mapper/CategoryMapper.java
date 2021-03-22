3b1194629a5300b338eec7279c875b3aa1f40445
Sephinor <w229084881@vip.qq.com>
2021-03-22 21:59:56 +0800

    查看品类Category信息2

--- kanglong-service/src/main/java/com/sephinor/kanglong/mapper/CategoryMapper.java
+++ kanglong-service/src/main/java/com/sephinor/kanglong/mapper/CategoryMapper.java
@@ -24,7 +24,8 @@ public interface CategoryMapper extends Mapper<Category> , IdListMapper<Category
     /**
      * 查询指定品牌所属的所有品类
      */
-    @Select("SELECT c.* FROM `t_category` c INNER JOIN  `t_category_brand` cb ON c.`id` = cb.`category_id` WHERE cb.`brand_id` = #{bid}")
-   List<Category> findByBrandId(@Param("bid") Long bid) ;
+    @Select("select t3.id id , CONCAT(t1.name,' / ', t2.name,' / ' , t3.name) name from t_category t1 LEFT OUTER JOIN t_category t2 on t1.id = t2.parent_id LEFT OUTER JOIN t_category t3 on t2.id = t3.parent_id WHERE t1.parent_id = 0 and t3.id in (select t.category_id from t_category_brand t where t.brand_id = #{bid})")
+    List<Category> findByBrandId(@Param("bid") Long bid) ;
+
 
 }
