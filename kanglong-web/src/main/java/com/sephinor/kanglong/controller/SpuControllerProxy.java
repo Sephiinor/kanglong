ebf440e1b5f752c9cb89085a4ccb92793c3233b2
Sephinor <w229084881@vip.qq.com>
2021-04-27 20:19:53 +0800

    保存商品参数组装

--- kanglong-web/src/main/java/com/sephinor/kanglong/controller/SpuControllerProxy.java
+++ kanglong-web/src/main/java/com/sephinor/kanglong/controller/SpuControllerProxy.java
@@ -1,6 +1,9 @@
 package com.sephinor.kanglong.controller;
 
 
+import com.sephinor.common.entity.Sku;
+import com.sephinor.common.entity.Spu;
+import com.sephinor.common.entity.SpuDetail;
 import com.sephinor.common.vo.CategoryVO;
 import com.sephinor.common.vo.SpuVO;
 import com.sephinor.kangkong.service.api.BrandServiceApi;
@@ -11,9 +14,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -47,6 +55,90 @@ public class SpuControllerProxy {
 
         return "spu/Manage";
     }
+
+
+    /**
+     *  新增或保存
+     * @param spu
+     * @param spuDetail
+     * @param request
+     */
+    @PostMapping("/saveOrUpdate")
+    @ResponseBody
+    public void saveOrUpdate(Spu spu , SpuDetail spuDetail , HttpServletRequest request){
+        //取得所有spu
+        Map<String , String[]> map =request.getParameterMap();
+        String[] skus = request.getParameterValues("sku");
+        Sku sku = null ;
+        Date date = new Date();
+        for (String s: skus ) {
+            sku = new Sku();
+            if(spu.getId() == null){
+                sku.setCreateTime(date);
+            }
+            sku.setLastUpdateTime(date);
+            sku.setEnable(getBoolean(map , s+".enable"));
+            sku.setId(getLong(map , s+".id"));
+            sku.setPrice(getLong(map , s+".price"));
+            sku.setTitle(getString(map , s+".title"));
+            sku.setOwnSpec(s);
+            sku.setStock(getInteger(map,s+".stock"));
+
+        }
+        System.out.println("sku ========"+sku);
+
+    }
+
+    //从map中提取布尔值
+    private Boolean getBoolean(Map<String ,String[]> map,String key){
+        String[] values= map.get(key);
+        if(values != null && values.length > 0){
+            String str = values[0];
+            if(null != str && !"".equals(str.trim())){
+                return Boolean.parseBoolean(str);
+
+            }
+        }
+        return null ;
+    }
+
+    //从map中提取Int值
+    private Integer getInteger(Map<String ,String[]> map,String key){
+        String[] values= map.get(key);
+        if(values != null && values.length > 0){
+            String str = values[0];
+            if(null != str && !"".equals(str.trim())){
+                return Integer.parseInt(str);
+
+            }
+        }
+        return null ;
+    }
+    //从map中提取Long值
+    private Long getLong(Map<String ,String[]> map,String key){
+        String[] values= map.get(key);
+        if(values != null && values.length > 0){
+            String str = values[0];
+            if(null != str && !"".equals(str.trim())){
+                return Long.parseLong(str);
+
+            }
+        }
+        return null ;
+    }
+    //从map中提取Long值
+    private String getString(Map<String ,String[]> map,String key){
+        String[] values= map.get(key);
+        if(values != null && values.length > 0){
+            String str = values[0];
+            if(null != str && !"".equals(str.trim())){
+                return str.trim();
+
+            }
+        }
+        return null ;
+    }
+
 
 
 }
